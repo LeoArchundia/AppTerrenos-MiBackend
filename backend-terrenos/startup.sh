9c882ba9eb8b34b9ffdada14ed4bd0840247eee3
LeoArchundia <leonmarch1303@gmail.com>
2025-12-15 21:15:06 -0600

    Fix: Añadir variable OPENSSL_CONF=/dev/null en startup.sh para resolver error de libproviders.so en html-pdf.

--- backend-terrenos/startup.sh
+++ backend-terrenos/startup.sh
@@ -4,9 +4,12 @@
 # 1. Instalar las librerías del sistema operativo requeridas por PhantomJS
 apt-get update && apt-get install -y libfontconfig libfreetype6
 
-# 2. Navegar al directorio de la aplicación
+# 2. **FIX DE OPENSSL/html-pdf**
+# Esto previene el error "libproviders.so: cannot open shared object file"
+export OPENSSL_CONF=/dev/null 
+
+# 3. Navegar al directorio de la aplicación
 cd /home/site/wwwroot/backend-terrenos
 
-# 3. Iniciar la aplicación Node.js
-# Asegúrate de que este comando coincide con tu script 'start' en package.json
+# 4. Iniciar la aplicación Node.js
 npm start
