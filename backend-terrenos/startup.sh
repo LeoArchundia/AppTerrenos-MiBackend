cc60a7782f7fd0bc79a6101bcef3815e14e6e1a9
LeoArchundia <leonmarch1303@gmail.com>
2025-12-15 20:28:27 -0600

    Feature: Añadir startup.sh y configuracion para instalar dependencias de Linux (libfontconfig).

---
+++ backend-terrenos/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script para instalar dependencias de html-pdf (PhantomJS)
+
+# 1. Instalar las librerías del sistema operativo requeridas por PhantomJS
+apt-get update && apt-get install -y libfontconfig libfreetype6
+
+# 2. Iniciar la aplicación Node.js
+# Asegúrate de que este comando coincide con tu script 'start' en package.json
+npm start
