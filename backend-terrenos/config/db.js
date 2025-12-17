1b4a30c289ce4b5010f24533711a5fcdbe1882b2
LeoArchundia <leonmarch1303@gmail.com>
2025-12-16 22:50:16 -0600

    Update db.js

--- backend-terrenos/config/db.js
+++ backend-terrenos/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER, // Ejemplo: proyectoterrenos.database.windows.net
+    server: process.env.DB_HOST, // Ejemplo: proyectoterrenos.database.windows.net
     database: process.env.DB_NAME,
     options: {
         encrypt: true, // Obligatorio para Azure
